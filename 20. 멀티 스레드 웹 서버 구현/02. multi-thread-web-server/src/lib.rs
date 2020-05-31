406f8ba69f52a107c185833a922a3deeae947480
hypernova1 <hypemova@gmail.com>
2020-05-31 13:26:53 +0900

    멀티스레드 서버 학습

--- 20. 멀티 스레드 웹 서버 구현/02. multi-thread-web-server/src/lib.rs
+++ 20. 멀티 스레드 웹 서버 구현/02. multi-thread-web-server/src/lib.rs
@@ -1,8 +1,24 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
 
 impl ThreadPool {
     /// 새 스레드풀 생성
@@ -15,22 +31,57 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
 
-        for _ in 1..size {
+        let receiver = Arc::new(Mutex::new(receiver));
 
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
-            threads
+            workers,
+            sender,
         }
     }
 
-    //클로저를 한 번만 실행하기 때문에 FnOnce(), 클로저가 스레드에서 스레드로 이동을 해야하기 때문에 Send, 스레드가 언제 파괴될 지 모르기 때문에 'static
+    //클로저를 한 번만 실행하기 때문에 FnOnce(), 클로저가 스레드에서 스레드로 이동을 해야하기 때문에 Send, 스레드가 언제 drop 될 지 모르기 때문에 'static
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
+    }
+}
+
+/*
+    스레드를 생성하고 코드를 전달 받을 때 까지 기다리도록 해야하는데,
+    표준 라이브러리의 스레드 구현에는 이런 방법을 지원하지 않아 직접 Worker 로 구현
+*/
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+
+                job.call_box();
+            }
+        });
+
+        Worker {
+            id,
+            thread,
+        }
     }
 }
